ac247f80455b5f42f670c4587126dd37d2065bd7
VDaniilO <91567079+VDaniilO@users.noreply.github.com>
2022-05-23 10:23:00 +0300

    Update urls.py
    fix out forms

--- FormProject_data/data_app/urls.py
+++ FormProject_data/data_app/urls.py
@@ -6,6 +6,6 @@ from .views import *
 urlpatterns = [
     path('main/', form_out, name='all-form'),
     path('main/add_form', add_form, name='add-form'),
-    path('main/edit_form/<int:form_number>', edit_form, name='edit-form'),
+    path('main/edit_form/<int:form_uid>', edit_form, name='edit-form'),
     path('main/add_fields/', add_field, name='add-fields')
 ]
